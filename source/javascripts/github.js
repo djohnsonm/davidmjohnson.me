4c2f3770c3148e4af0dcfa235c9d0a5d38a3444e
David Johnson <djohnson.m@gmail.com>
2012-08-28 21:52:14 -0500

    fixed github url error

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -2,9 +2,9 @@ var github = (function(){
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
-    for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
-    }
+      for(i = 0; i < repos.length; i++) {
+        fragment += '<li><a href="'+repos[i].html_url +'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+      }
     t.innerHTML = fragment;
   }
   return {
